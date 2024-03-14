fdf8b0346dadb8f1762e1380c5e1e7b34e3ed575
Zee-Yeon <122084111+Zee-Yeon@users.noreply.github.com>
2024-03-15 01:18:18 +0900

    oauth + jwt

---
+++ oauth/src/main/java/com/jiyeon/oauth/domain/user/SocialType.java
@@ -0,0 +1,5 @@
+package com.jiyeon.oauth.domain.user;
+
+public enum SocialType {
+    KAKAO, NAVER, GOOGLE
+}
---
+++ oauth/src/main/java/com/jiyeon/oauth/domain/user/service/UserService.java
@@ -0,0 +1,44 @@
+package com.jiyeon.oauth.domain.user.service;
+
+import com.jiyeon.oauth.domain.user.Role;
+import com.jiyeon.oauth.domain.user.User;
+import com.jiyeon.oauth.domain.user.dto.UserSignUpDto;
+import com.jiyeon.oauth.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signUp(UserSignUpDto userSignUpDto) throws Exception {
+
+        if (userRepository.findByEmail(userSignUpDto.getEmail()).isPresent()) {
+            throw new Exception("이미 존재하는 이메일입니다.");
+        }
+
+        if (userRepository.findByNickname(userSignUpDto.getNickname()).isPresent()) {
+            throw new Exception("이미 존재하는 닉네임입니다.");
+        }
+
+        User user = User.builder()
+                .email(userSignUpDto.getEmail())
+                .password(userSignUpDto.getPassword())
+                .nickname(userSignUpDto.getNickname())
+                .age(userSignUpDto.getAge())
+                .city(userSignUpDto.getCity())
+                .role(Role.USER)
+                .build();
+
+        user.passwordEncode(passwordEncoder);
+        userRepository.save(user);
+    }
+}
+
+
